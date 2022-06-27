0bf946ab2ce87a1f4b14016e84f56dc5d029ee63
Abdullah Gür <14290100@ogrenci.ankara.edu.tr>
2022-06-27 23:05:22 +0300

    read dictionary text file

--- src/Word.java
+++ src/Word.java
@@ -16,8 +16,23 @@ abstract class Word {
     }
 
     // get the words that the length is equal to determining size
-    String function(int size) {
+    String function(int size) throws IOException {
 
-        return "";
+        File file = new File("deneme.txt");
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String line;
+
+        while((line = bufferedReader.readLine()) != null)
+        {
+
+            String[] line2 = line.split("\\s+"); // take only first split from specific line
+
+            if(size == line2[0].length())
+                System.out.println(line2[0] + " " + line2[0].length());
+        }
+        fileReader.close();
+
+        return line;
     }
 }
